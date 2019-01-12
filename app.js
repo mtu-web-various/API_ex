64382af5b624cb052638d4ea8e437319be091d47
Talha Ucar <talhamucar@gmail.com>
2019-01-12 18:18:20 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = process.env.PORT || 3000;
+
+var todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+app.use('/api/todos', todoRoutes);
+
+app.get('/', function(req, res){
+   res.sendFile("index.html"); 
+});
+
+app.listen(port, function(){
+   console.log("server has started..."); 
+});
